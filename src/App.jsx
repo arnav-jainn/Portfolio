0fba9a97a454e654559d5c7c03d83e48539e9f08
Arnav jain <arnavjainn02@gmail.com>
2024-08-20 23:05:00 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About/About'
 import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Skills/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
